6e980d54bd733f782bda0d385d74c9198b3548a4
Harald Lapp <h.lapp@clipdealer.de>
2014-10-31 19:32:56 +0100

    updated copyright year after touching probably all files

--- libs/Db/Device/Mysql/Connection.php
+++ libs/Db/Device/Mysql/Connection.php
@@ -14,7 +14,7 @@ namespace Octris\Core\Db\Device\Mysql;
 /**
  * MySQL connection handler.
  *
- * @copyright   copyright (c) 2012 by Harald Lapp
+ * @copyright   copyright (c) 2012-2014 by Harald Lapp
  * @author      Harald Lapp <harald@octris.org>
  */
 class Connection extends \mysqli implements \Octris\Core\Db\Device\IConnection, \octris\core\db\pool_if
