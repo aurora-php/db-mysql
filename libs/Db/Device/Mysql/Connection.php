a419508464474ca3d3695429a0fdeebcd157269f
Harald Lapp <harald@octris.org>
2016-01-05 02:49:09 +0100

    fixed namespaces

--- libs/Db/Device/Mysql/Connection.php
+++ libs/Db/Device/Mysql/Connection.php
@@ -94,15 +94,15 @@ class Connection extends \mysqli implements \Octris\Core\Db\Device\IConnection
 
     /**
      * Query the database. The query will handle deadlocks and perform several tries up
-     * to \Octris\Core\Db\Mysql::DEADLOCK_ATTEMPTS until a deadlock is considered
+     * to \Octris\Core\Db\Device\Mysql::DEADLOCK_ATTEMPTS until a deadlock is considered
      * to be unresolvable.
      *
      * @param   string              $sql                    SQL query to perform.
-     * @return  \Octris\Core\Db\Mysql\Result            Query result.
+     * @return  \Octris\Core\Db\Device\Mysql\Result         Query result.
      */
     public function query($sql)
     {
-        for ($i = 0; $i < \Octris\Core\Db\Mysql::DEADLOCK_ATTEMPTS; ++$i) {
+        for ($i = 0; $i < \Octris\Core\Db\Device\Mysql::DEADLOCK_ATTEMPTS; ++$i) {
             $res = $this->real_query($sql);
 
             if ($res !== false || ($this->errno != 1205 && $this->errno != 1213)) {
@@ -114,20 +114,20 @@ class Connection extends \mysqli implements \Octris\Core\Db\Device\IConnection
             throw new \Exception($this->error, $this->errno);
         }
 
-        return new \Octris\Core\Db\Mysql\Result($this);
+        return new \Octris\Core\Db\Device\Mysql\Result($this);
     }
 
     /**
      * Performs asynchronous query.
      *
      * @param   string              $sql                    SQL query to perform.
-     * @return  \Octris\Core\Db\Mysql\Async             Asynchronous query object.
+     * @return  \Octris\Core\Db\Device\Mysql\Async          Asynchronous query object.
      */
     public function asyncQuery($sql)
     {
         $this->query($sql, MYSQLI_ASYNC);
 
-        $async = \Octris\Core\Db\Mysql\Async::getInstance();
+        $async = \Octris\Core\Db\Device\Mysql\Async::getInstance();
         $async->addLink($this);
 
         return $async;
@@ -152,18 +152,18 @@ class Connection extends \mysqli implements \Octris\Core\Db\Device\IConnection
             throw new \Exception($this->error, $this->errno);
         }
 
-        return new \Octris\Core\Db\Mysql\Result($this);
+        return new \Octris\Core\Db\Device\Mysql\Result($this);
     }
 
     /**
      * Initialize prepared statement.
      *
      * @param   string              $sql                    SQL query to prepare.
-     * @return  \Octris\Core\Db\Mysql\Statement         Instance of a prepared statement.
+     * @return  \Octris\Core\Db\Device\Mysql\Statement      Instance of a prepared statement.
      */
     public function prepare($sql)
     {
-        $stmt = new \Octris\Core\Db\Mysql\Statement($this, $sql);
+        $stmt = new \Octris\Core\Db\Device\Mysql\Statement($this, $sql);
 
         if ($stmt->errno > 0) {
             throw new \Exception($stmt->sqlstate . ' ' . $stmt->error, $stmt->errno);
